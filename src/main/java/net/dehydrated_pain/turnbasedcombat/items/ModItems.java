74713aca148b5067f6bb5665767f91e563abd09d
XinshenXiangChen <xinshenxiangchen@gmail.com>
2026-01-01 22:49:53 +0100

    moditems

---
+++ src/main/java/net/dehydrated_pain/turnbasedcombat/items/ModItems.java
@@ -0,0 +1,8 @@
+package net.dehydrated_pain.turnbasedcombat.items;
+
+import net.dehydrated_pain.turnbasedcombat.TurnBasedCombatMod;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(TurnBasedCombatMod.MODID)
+}
